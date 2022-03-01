ce75132682541f43ac61915b2c3ad35e5b9eb39a
Kevin-CrA <a1117140138@utch.edu.mx>
2022-02-28 18:23:49 -0700

    Fixing extra line

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home'
 import DetailCharacter from './pages/detail/DetailCharacter'
-import Sample from './components/sample/sample';
 import './App.css';
 
 const App = () =>{
